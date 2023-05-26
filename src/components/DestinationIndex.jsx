b338cbd8ab938113478e69aaaf8d82cc65f99e7b
sdin <sdin@LAPTOP-DINESH>
2023-05-26 14:16:12 +0200

    fetch records from api

--- src/components/DestinationIndex.jsx
+++ src/components/DestinationIndex.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import AddDestination from './AddDestination'
+import DestinationList from './DestinationList'
 
 function DestinationIndex() {
   return (
     <div className="text-white-50">
         <h3 className="text-success">Travel List</h3>
         <AddDestination/>
+        <DestinationList/>
     </div>
   )
 }
