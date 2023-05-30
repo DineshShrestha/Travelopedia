6ad4b5ec4103afe0a8e331f82713c5b768605a21
sdin <sdin@LAPTOP-DINESH>
2023-05-30 12:00:44 +0200

    seperating out individual components

--- src/components/DestinationList.jsx
+++ src/components/DestinationList.jsx
@@ -1,26 +1,17 @@
 import React from 'react'
 import { useGetAllDestinationQuery } from '../api/DestinationApi'
-import { useDeleteDestinationMutation } from '../api/DestinationApi';
+import Destination from './Destination'
+
 function DestinationList() {
     const {data, isLoading, isSuccess, isError, error}=useGetAllDestinationQuery();
-    const [deleteDestination] = useDeleteDestinationMutation();
+    
     let content;
     if(isLoading){
         content = <p>Loading...</p>;
         } else if (isSuccess) {
             content =data.map((destination)=>{
                 return(
-                    <div className='row py-1' style={{borderBottom: '1px solid #333', borderTop:'1px solid #333'}} key={destination.id}>
-                        <div className="col-3 offset-3">
-                                {destination.city}, {destination.country}                           
-                        </div>
-                        <div className='col-1 text-warning'>
-                            {destination.daysNeeded} days     
-                        </div>
-                        <div className='col-2'>
-                        <button className='btn btn-danger form-control' onClick={()=>deleteDestination({id: destination.id})}>Delete</button>
-                        </div>
-                    </div>
+                    <Destination destination={destination}  key={destination.id}/>
                 )
             })
         }else if(isError){
