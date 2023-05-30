071872fabc741b6a5cbe3755b54124870878edde
sdin <sdin@LAPTOP-DINESH>
2023-05-30 11:39:28 +0200

    delete destination

--- src/components/DestinationList.jsx
+++ src/components/DestinationList.jsx
@@ -1,21 +1,26 @@
 import React from 'react'
 import { useGetAllDestinationQuery } from '../api/DestinationApi'
+import { useDeleteDestinationMutation } from '../api/DestinationApi';
 function DestinationList() {
     const {data, isLoading, isSuccess, isError, error}=useGetAllDestinationQuery();
+    const [deleteDestination] = useDeleteDestinationMutation();
     let content;
     if(isLoading){
-        content = <p>Loading...</p>
+        content = <p>Loading...</p>;
         } else if (isSuccess) {
             content =data.map((destination)=>{
                 return(
-                    <article key={destination.id}>
-                        <div className="text-center text-info p-2">
-                            <div>
-                                {destination.city}, {destination.country} - {" "}
-                                {destination.daysNeeded} days
-                            </div>
+                    <div className='row py-1' style={{borderBottom: '1px solid #333', borderTop:'1px solid #333'}} key={destination.id}>
+                        <div className="col-3 offset-3">
+                                {destination.city}, {destination.country}                           
                         </div>
-                    </article>
+                        <div className='col-1 text-warning'>
+                            {destination.daysNeeded} days     
+                        </div>
+                        <div className='col-2'>
+                        <button className='btn btn-danger form-control' onClick={()=>deleteDestination({id: destination.id})}>Delete</button>
+                        </div>
+                    </div>
                 )
             })
         }else if(isError){
