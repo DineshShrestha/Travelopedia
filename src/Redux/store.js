127e703450ca40a443d550bb0eb49981ce27f525
sdin <sdin@LAPTOP-DINESH>
2023-05-30 14:17:02 +0200

    Show random destination

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,9 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { destinationApi } from "../api/DestinationApi";
-
+import { RandomDestinationApi } from "../api/RandomDestinationApi";
 export const store = configureStore({
     reducer:{
-        [destinationApi.reducerPath]: destinationApi.reducer
+        [destinationApi.reducerPath]: destinationApi.reducer,
+        [RandomDestinationApi.reducerPath]: RandomDestinationApi.reducer
     },
-    middleware: (getDefaultMiddleware)=>getDefaultMiddleware().concat(destinationApi.middleware),
+    middleware: (getDefaultMiddleware)=>getDefaultMiddleware().concat(destinationApi.middleware).concat(RandomDestinationApi.middleware),
 })
---
+++ src/api/RandomDestinationApi.jsx
@@ -0,0 +1,19 @@
+import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
+
+export const RandomDestinationApi = createApi({
+    reducerPath: "apiRandomDestination",
+    baseQuery : fetchBaseQuery({baseUrl: "https://random-data-api.com/api/v2/"}),
+    endpoints: (builder)=> ({
+        getRandomDestination: builder.query({
+            query: ()=>({
+                url: "addresses",
+                method: "GET",
+            }),
+        }),
+    }),
+});
+
+
+export const { 
+                useGetRandomDestinationQuery, 
+            } = RandomDestinationApi;
